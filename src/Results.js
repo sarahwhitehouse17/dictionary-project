4a7bab284c88c84a1cd0862d941633ba10c58d69
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-01-09 16:28:58 +0100

    Made Meaning and Results to display results

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Meaning from "./Meaning";
 
-//create an if statement
-//change to null if it doesn't work
+//create a map to map through the meaning.
+//create a new component called meaning to display the content
 
 export default function Result(props) {
+  console.log(props.results);
   if (props.results) {
-    return <h2>{props.results.word}</h2>;
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
