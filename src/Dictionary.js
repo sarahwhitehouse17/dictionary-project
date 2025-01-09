b50a504188214538cae8da24f0a80ac0a71860d5
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-01-09 11:52:38 +0100

    Added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
-//Create form
-//Funcion for onSubmit
-//Funcion for onChange
-//console.log the value to check all linked up
+//Add in the She cODES api to handle submit.
+//Axios. get to use the API and create the function
+// With the function, console.log the response to check it's working
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+
+    //Api resource - https://api.shecodes.io/dictionary
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=3deb07c475cdt015b4f399a253e4o0b8`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
