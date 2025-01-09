d764d4b5dc843bcbc7bfe4364a9180dc7814a002
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-01-09 10:39:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="./" className="btn btn-primary">
+          Hello
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
