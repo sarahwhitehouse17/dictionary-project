25f8053051b2b3811f78630f0095d1c22d07e470
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-01-10 11:14:17 +0100

    Added synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,12 @@ export default function Synonyms(props) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          return (
+            <div>
+              <h4>Synonyms:</h4>
+              <li key={index}>{synonym}</li>
+            </div>
+          );
         })}
       </ul>
     );
