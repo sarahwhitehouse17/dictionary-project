373a87f8e0fe838820c1d65c0f4bcc87406d67d2
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-01-10 10:37:35 +0100

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
+  console.log(props.meaning.synonyms);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
+      < Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 
-//create a map to map through the meaning.
-//create a new component called meaning to display the content
-
 export default function Result(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
